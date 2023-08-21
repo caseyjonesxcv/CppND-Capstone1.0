e6cefc677b7739da7c1909fa0a285c0c5ac300d7
cjonesnine2 <138345643+cjonesnine2@users.noreply.github.com>
2023-08-21 04:17:31 +0000

    Add files via upload

---
+++ CppND-Capstone/src/alien.cpp
@@ -0,0 +1,33 @@
+#include "alien.h"
+#include <cmath>
+#include <iostream>
+
+/*
+void Alien::Update() {
+    int x, y;
+    while (true) {
+    x = random_w(engine);
+    y = 0;
+    // Check that the location is not occupied by a snake item before placing
+    // food.
+    if (!Snake::snake.SnakeCell(x, y)) {
+      alien.x = x;
+      alien.y = y;
+      return;
+    }
+  }
+}
+
+  // Wrap the Snake around to the beginning if going off of the screen.
+  //head_x = fmod(head_x + grid_width, grid_width);
+  //head_y = fmod(head_y + grid_height, grid_height);
+}
+
+// Inefficient method to check if cell is occupied by snake.
+
+bool Alien::AlienCell(int x, int y) {
+  if (x == static_cast<int>(head_x) && y == static_cast<int>(head_y)) {
+    return true;
+  }
+  return false;
+}*/
---
+++ CppND-Capstone/src/alien.h
@@ -0,0 +1,29 @@
+#ifndef ALIEN_H
+#define ALIEN_H
+
+#include <vector>
+#include "SDL.h"
+/*
+class Alien {
+ public:
+  enum class Direction { kNone, kUp, kDown, kLeft, kRight };
+
+  Alien() {}
+
+  void Update();
+
+  //bool AlienCell(int x, int y);
+
+  float speed{.09};
+  int size{1};
+  bool alive{true};
+  float locationX;
+  float locationY;
+
+ private:
+  SDL_Point alien;
+  int grid_width;
+  int grid_height;
+};
+*/
+#endif
